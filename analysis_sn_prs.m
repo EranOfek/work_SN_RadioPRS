638003624cf155df45634d4a44e2a9fc3449887c
EranOfek <ofek.eran@gmail.com>
2026-04-25 21:56:44 +0300

    with new par

--- analysis_sn_prs.m
+++ analysis_sn_prs.m
@@ -94,20 +94,50 @@ TNS.VLASS_Fpeak= MatchedRadio(:,7);
 TNS.VLASS_ErrFpeak = MatchedRadio(:,8);
 TNS.VLASS_Dist     = MatchedDist(:,1);
 Fps = ~isnan(MatchedRadio(:,5)) & MatchedRadio(:,5)< (MatchedRadio(:,7) + 3.* sqrt(MatchedRadio(:,6).^2 + MatchedRadio(:,8).^2));
+% old compactness
 Compactness = MatchedRadio(:,5) ./ (MatchedRadio(:,7) + 3.* sqrt(MatchedRadio(:,6).^2 + MatchedRadio(:,8).^2)); % compact are <1
 
-TNS.Compactness = Compactness;
-sum(Compactness<1)
+% new compactness
+
+% for all VLASS
+F_total = VLASS.Table.Ftot;
+F_peak  = VLASS.Table.Fpeak;
+DeltaF_total = VLASS.Table.ErrFtot;
+DeltaF_peak  = VLASS.Table.ErrFpeak;
+
+CV = ((F_total./F_peak) - 1) ./ ...
+    (abs(F_total./F_peak) .* sqrt((DeltaF_total./F_total).^2 + (DeltaF_peak./F_peak).^2));
+
+
+
+% for TNS
+F_total = TNS.VLASS_Ftot;
+F_peak  = TNS.VLASS_Fpeak;
+DeltaF_total = MatchedRadio(:,6);
+DeltaF_peak  = TNS.VLASS_ErrFpeak;
+
+
+C = ((F_total./F_peak) - 1) ./ ...
+    (abs(F_total./F_peak) .* sqrt((DeltaF_total./F_total).^2 + (DeltaF_peak./F_peak).^2));
+
+TNS.Compactness = C;
+
+
 % 2236 selected (with Z) / 2912 (all) 
 
-VLASS = cats.radio.VLASS1;
-FPS=~isnan(VLASS.Table.Ftot) & VLASS.Table.Ftot<(VLASS.Table.Fpeak + 3.*sqrt(VLASS.Table.ErrFtot.^2 + VLASS.Table.ErrFpeak));
-sum(FPS)./size(VLASS.Catalog,1)
-% 81% are point sources (with Z) / 81% (all)
 
 % select clear associations (<1"):
-Fd=MatchedDist(:,1)<3.3; 
+Fd=MatchedDist(:,1)<1; 
 % 1343 unique matches (with Z) / 2420 (all)
+
+
+%VLASS = cats.radio.VLASS1;
+%FPS=~isnan(VLASS.Table.Ftot) & VLASS.Table.Ftot<(VLASS.Table.Fpeak + 3.*sqrt(VLASS.Table.ErrFtot.^2 + VLASS.Table.ErrFpeak));
+%sum(FPS)./size(VLASS.Catalog,1)
+sum(CV<2)./size(VLASS.Catalog,1)
+% 41% are point sources (with Z) / 81% (all)
+sum(C<2 & Fd)./sum(Fd)
+% 37%
 
 % redshift
 Fz = TNS.Z<0.1;
@@ -117,15 +147,15 @@ Fz = TNS.Z<0.1;
 Ftime = TNS.discoverydate.Year<2017;
 % 3973 matches (with Z) / 7588 (all)
 
-FF = Compactness<1 & Fd & Fz & Ftime;
+FF = C<2 & Fd & Fz & Ftime;
 sum(FF)
 % 10 matches
 
 % we are interested in all events
 FF = Fd & Ftime;           
 sum(FF)
-% 77 matches (with 1'') / 120 with 3.3''
-
+% 86 matches (with 1.25'') / 120 with 3.3''
+s
 
 % all are Ic-BL !
 %TNS.type(FF)          
@@ -136,6 +166,8 @@ sum(FF)
 
 % abs mag:
 TNS.disc_abs_mag = TNS.discoverymag - (5.*log10(astro.cosmo.lum_dist(TNS.Z))-5);
+
+%%
 
 save -v7.3 TNS_Selected.mat TNS
 
@@ -194,13 +226,64 @@ H.Interpreter = 'latex';
 
 print RadioCompactness_Tran.eps -depsc2
 
+CC = C(Fd);
+EdgesC = (-3:0.5:8);
+BinC   = (EdgesC(1:end-1) + EdgesC(2:end)).*0.5;
+Nc=histcounts(CC,EdgesC);
+Ncv=histcounts(CV,EdgesC);
+bar(BinC, [Ncv(:)./sum(Ncv), Nc(:)./sum(Nc)]);
+H = xlabel('Compactness');
+H.FontSize = 18;
+H.Interpreter = 'latex';
+H = ylabel('Fraction');
+H.FontSize = 18;
+H.Interpreter = 'latex';
 
+legend('All','TNS');
+
+print RadioCompactness_All.eps -depsc2
 
 
 %%
-TNS_FF   = TNS(FF,:);
+TNS_FF   = TNS(C<2 & MatchedDist(:,1)<3.3 & Ftime, :);
 TNS_FF   = sortrows(TNS_FF, {'VLASS_Dist'});
 
-Selected = TNS_FF(:,{'name','type','ra','declination','Z','discoverymag','internal_names','disc_abs_mag', 'VLASS_Fpeak', 'VLASS_ErrFpeak', 'VLASS_Dist'});
-tools.table.sprintf_table(Selected,'Format',{'%-7s','%-9s','%10.6f','%10.6f','%6.4f','%4.1f','%-35s','%6.1f', '%6.1f', '%6.1f', '%3.1f'},'IsLatex',true)
+% match to external catalogs:
+NVSS  = cats.radio.NVSS;
+FIRST = cats.radio.FIRST;
+EROSITA = cats.X.eRosita;
+Nff     = size(TNS_FF,1);
+TNS_FF.NVSS = nan(Nff,1);
+TNS_FF.FIRST = nan(Nff,1);
+TNS_FF.EROSITRA = nan(Nff,1);
+
+for Iff=1:1:Nff
+    DistAll = celestial.coo.sphere_dist_fast(TNS_FF.ra(Iff)./RAD, TNS_FF.declination(Iff)./RAD, NVSS.Cat(:,1), NVSS.Cat(:,2));
+    [MinDist, MinInd] = min(DistAll);
+    MinDist = MinDist.*RAD.*3600;
+    if MinDist<10
+        TNS_FF.NVSS(Iff) = NVSS.Cat(MinInd, 5);
+    end
+    
+    DistAll = celestial.coo.sphere_dist_fast(TNS_FF.ra(Iff)./RAD, TNS_FF.declination(Iff)./RAD, FIRST.Cat(:,1), FIRST.Cat(:,2));
+    [MinDist, MinInd] = min(DistAll);
+    MinDist = MinDist.*RAD.*3600;
+    if MinDist<1.5
+        TNS_FF.FIRST(Iff) = FIRST.Cat(MinInd, 4);
+    end
+    
+    DistAll = celestial.coo.sphere_dist_fast(TNS_FF.ra(Iff)./RAD, TNS_FF.declination(Iff)./RAD, EROSITA.Catalog(:,1), EROSITA.Catalog(:,2));
+    [MinDist, MinInd] = min(DistAll);
+    MinDist = MinDist.*RAD.*3600;
+    if MinDist<7
+        TNS_FF.EROSITA(Iff) = EROSITA.Catalog(MinInd, 14); % flux 0.2-2.3
+    end
+
+end
+
+TNS_FF.EROSITA(TNS_FF.EROSITA == 0) = NaN;
+TNS_FF.nuFnu = 3e9.*TNS_FF.VLASS_Fpeak.*4.*pi.*(astro.cosmo.lum_dist(TNS_FF.Z).*constant.pc).^2 .*1e-26;
+
+Selected = TNS_FF(:,{'name','type','ra','declination','Z','discoverymag','disc_abs_mag', 'VLASS_Fpeak', 'VLASS_ErrFpeak', 'VLASS_Dist', 'nuFnu', 'NVSS', 'FIRST','EROSITA'});
+tools.table.sprintf_table(Selected,'Format',{'%-7s','%-9s','%10.6f','%10.6f','%6.4f','%4.1f','%6.1f', '%6.1f', '%6.1f', '%3.1f', '%5.1e', '%5.1f', '%5.1f', '%4.1e'},'IsLatex',true, 'NoData',true)
 
